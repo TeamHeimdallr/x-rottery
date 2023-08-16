b1224708fa85fbbfb2867ede7fcb926ea9906b4a
siwonblue <siwonblue@icloud.com>
2023-08-16 18:44:44 +0900

    [style] add cancle button

--- src/components/buttons/cancle-button.tsx
+++ src/components/buttons/cancle-button.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import tw from 'twin.macro';
 
+import { COLOR } from '~/assets/colors';
+
+import { IconCancle } from '../icons';
+
 export const CancleButton = () => {
-  return <Wrapper></Wrapper>;
+  const [isHover, setIsHover] = useState(false);
+  return (
+    <Wrapper onMouseOver={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+      <IconCancle color={isHover ? COLOR.GRAY1 : COLOR.GRAY2} />
+    </Wrapper>
+  );
 };
 
 const Wrapper = tw.div`
-  w-40 h-40 rounded-full
+  w-40 h-40 flex-center clickable rounded-full
+  bg-gray5
+  hover:(bg-gray4)
 `;
