8a0cca6852e3549d1a42ea1160ea2da1d4062fcb
siwonblue <siwonblue@icloud.com>
2023-08-17 10:37:15 +0900

    [fix] apply code review

--- src/components/buttons/filled-medium.tsx
+++ src/components/buttons/filled-medium.tsx
@@ -42,18 +42,14 @@ const Wrapper = styled.button<ButtonProps>(({ isLoading }) => [
   tw`
     h-42 px-24 py-10 flex-center relative
     rounded-8 clickable main-gradient text-white
+    font-b-14
     hover:(text-pink bg-none box-shadow)
     disabled:(bg-black bg-none text-gray3 non-clickable hover:(bg-black text-gray3 shadow-none))
   `,
   isLoading && tw`non-clickable bg-none hover:(shadow-none)`,
 ]);
 
-const TextWrapper = styled.div<ButtonProps>(({ isLoading }) => [
-  tw`
-    font-b-14
-  `,
-  isLoading && tw`opacity-0`,
-]);
+const TextWrapper = styled.div<ButtonProps>(({ isLoading }) => [isLoading && tw`opacity-0`]);
 const LottieWrapper = tw.div`
   w-full h-full flex-center absolute absolute-center
 `;
