880af5f2ceffb0de2f64abb956376b6d8652ec2f
jihyune <rlawlcns96@gmail.com>
2023-08-17 14:30:59 +0900

    feat: get account from seed

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,22 @@
 import tw from 'twin.macro';
+import { Client, Wallet } from 'xrpl';
 
 import { FilledLargeButton } from '~/components/buttons';
 
 const MainPage = () => {
+  const getAccountFromSeeds = async () => {
+    const net = 'wss://s.altnet.rippletest.net:51233';
+    const client = new Client(net);
+    await client.connect();
+
+    const wallet = Wallet.fromSeed('sEdTxNu2nY3EW4iJ2Vg3Tzy3Smvjexu');
+    console.log(wallet);
+  };
+
   return (
     <Wrapper>
-      <FilledLargeButton text={'Connect Wallet'} isLoading={true} />
+      <FilledLargeButton text={'Connect Wallet'} onClick={getAccountFromSeeds} />
+      <FilledLargeButton text={'Connect Wallet'} onClick={getAccountFromSeeds} />
     </Wrapper>
   );
 };
